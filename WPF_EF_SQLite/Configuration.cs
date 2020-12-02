6e504a20498dad2f212af7d1d83be3bc0b34cbaa
fantasy178 <blue-sky-78217@hotmail.com>
2020-12-02 16:11:58 +0800

    資料庫 .使用動態路徑 .動態建表(Migrate)

---
+++ WPF_EF_SQLite/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.SQLite.EF6.Migrations;
+
+namespace WPF_EF_SQLite
+{
+    class Configuration : DbMigrationsConfiguration<MyDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+            this.SetSqlGenerator("System.Data.SQLite", new SQLiteMigrationSqlGenerator());
+        }
+
+        protected override void Seed(MyDbContext context)
+        {
+            //This method will be called after migrating to the latest version.
+
+            //You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //to avoid creating duplicate seed data.E.g.
+
+            //context.Employees
+            //       .AddOrUpdate(p => p.Name,
+            //                    new Employee {Name = "Andrew Peters"},
+            //                    new Employee {Name = "Brice Lambson"},
+            //                    new Employee {Name = "Rowan Miller"}
+            //                   );
+        }
+    }
+}
--- WPF_EF_SQLite/MyDbContext.cs
+++ WPF_EF_SQLite/MyDbContext.cs
@@ -10,9 +10,33 @@ namespace WPF_EF_SQLite
 {
     class MyDbContext : DbContext
     {
+        private static readonly bool[] s_migrated = { false };
+        private static string path;
         public DbSet<Axis> Axes { get; set; }
 
-        public MyDbContext(string dbPath) : base(new SQLiteConnection() { ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = dbPath, ForeignKeys = true }.ConnectionString }, true)
+        public MyDbContext() : base(new SQLiteConnection() { ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = path }.ConnectionString }, true)
+        {
+        }
+        public MyDbContext(string dbPath) : base(new SQLiteConnection() { ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = dbPath }.ConnectionString }, true)
+        {
+            path = dbPath;
+            Migrate();
+        }
+        private static void Migrate()
+        {
+            if (!s_migrated[0])
+            {
+                lock (s_migrated)
+                {
+                    if (!s_migrated[0])
+                    {
+                        Database.SetInitializer(new MigrateDatabaseToLatestVersion<MyDbContext, Configuration>());
+                        s_migrated[0] = true;
+                    }
+                }
+            }
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
         }
     }
